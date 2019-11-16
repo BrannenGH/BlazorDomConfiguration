0790ed3929c95146b96209ee44217253468f5a9f
Brannen Hall <Brannen@Hallb.me>
2019-11-16 13:40:36 -0600

    Add ability to load a configuration from a remote JSON file

---
+++ Fario.Extensions.Configuration.Test/RemoteJsonConfigurationTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fario.Extensions.Configuration.Test
+{
+    public class RemoteJsonConfigurationTests: ConfigurationTestBase
+    {
+    }
+}
